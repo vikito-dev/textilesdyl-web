d400471e84d393bb3858fc9b92bbc4ca039cdafe
vikito-dev <quispecurovictorantonio4@gmail.com>
2026-06-12 18:27:14 -0500

    fix: preset de vercel configurado correctamente

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,17 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+// 1. Creamos la configuración aparte y le decimos a TypeScript que es de tipo "any"
+const startConfig: any = {
+  server: {
+    preset: "vercel"
+  }
+};
+
 export default defineConfig({
   plugins: [
-    tanstackStart(),
+    // 2. Pasamos la variable fantasma sin que TypeScript se queje
+    tanstackStart(startConfig),
     viteReact(),     
     tailwindcss(),
   ],
